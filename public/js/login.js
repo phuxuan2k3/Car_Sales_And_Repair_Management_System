a5e89544061ef85cfb591b9ad4e396c6e9b3db46
21120170 <phuxuan.px2003@gmail.com>
2023-12-07 20:33:59 +0700

    Addition

--- public/js/login.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('form').on('submit', function (e) {
-        $rememberChbx = $($('form input[type="checkbox"]')[0]);
-        $rememberChbx.val($rememberChbx.prop('checked') == true ? 'true' : 'false');
-        console.log($rememberChbx.val());
-    });
-});
