5275425cda899725713380fac45d45b59738a308
Võ Chí Trung <147219006+ChisTrun@users.noreply.github.com>
2023-12-08 09:26:56 +0700

    Update site.r.js

--- routers/site.r.js
+++ routers/site.r.js
@@ -4,7 +4,7 @@ const siteController = require('../controllers/site.c');
 
 // LƯU Ý VỀ BẢN CHẤT
 // CÁC XÁC THỰC PHÂN QUYỀN PHẢI CÀI ĐẶT TRONG MIDDLEWARE RIÊNG ROUTER REQUIRE VÀO ĐỂ DÙNG
-// GIA HẠN COOKIES KHÔNG PHẢI LÚC NÀO CŨNG GIA HẠN, PHẢI GIA HẠNG TRONG CONTROLLER KHÔNG PHẢI ROUTER
+// GIA HẠN COOKIES KHÔNG PHẢI LÚC NÀO CŨNG GIA HẠN, PHẢI GIA HẠN TRONG CONTROLLER KHÔNG PHẢI ROUTER
 // DATABASE CHỈ ĐƯỢC GỌI Ở CONTROLLER ROUTER CHỈ CÓ CHỨC NĂNG DUY NHẤT LÀ ĐỊNH TUYẾN 
 
 router.get('/',siteController.getIndex);
