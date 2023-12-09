547d53eed02c38b2ec037f308646816246e9cd63
21120159 <vochitrung.pst@gmail.com>
2023-12-09 12:10:38 +0700

    Tách session ra config

--- config/configSession.js
+++ config/configSession.js
@@ -1,27 +1,35 @@
 const session = require('express-session');
 require('dotenv').config();
 const ENV = process.env;
-const pg = require('pg');
 const pgSession = require('connect-pg-simple')(session);
 
+
+const pg = require('pg');
+
 const pgPool = new pg.Pool({
+    host: ENV.DBHOST,
     database: ENV.DBNAME,
     user: ENV.DBUSERNAME,
     password: ENV.DBPASSWORD,
     port: ENV.DBPORT,
 });
+
 const store = new pgSession({
     pool: pgPool,
     tableName: 'UserSessions',
     createTableIfMissing: true
 });
 
-module.exports = session({
-    store,
-    secret: ENV.SESSIONIDSECRET,
-    cookie: {
-        maxAge: parseInt(ENV.DEFAULTTIMEACCESS)
-    },
-    saveUninitialized: true,
-    resave: false
-})
+const configSession = (app) => {
+    app.use(session({
+        store,
+        secret: ENV.SESSIONIDSECRET,
+        cookie: {
+            maxAge: parseInt(ENV.DEFAULTTIMEACCESS)
+        },
+        saveUninitialized: true,
+        resave: false
+    }))
+}
+
+module.exports = configSession;
