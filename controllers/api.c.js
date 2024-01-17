682dba54874510bede57636df5b14e6fffe2a4d3
21120170 <phuxuan.px2003@gmail.com>
2024-01-17 10:42:26 +0700

    bacsic user model

--- controllers/api.c.js
+++ controllers/api.c.js
@@ -5,32 +5,32 @@ const Car = require('../models/car');
 
 module.exports = {
     //Car API
-    getAllCar: tryCatch( async(req,res) => {
+    getAllCar: tryCatch(async (req, res) => {
         const data = await Car.getAll();
         res.json(data);
     }),
-    getAllType: tryCatch(async (req,res) => {
+    getAllType: tryCatch(async (req, res) => {
         const data = await Car.getAllType();
         res.json(data)
     }),
-    getAllBrand: tryCatch(async (req,res) => {
+    getAllBrand: tryCatch(async (req, res) => {
         const data = await Car.getAllBrand();
         res.json(data)
     }),
-    getCarPage: tryCatch(async (req,res) => {
+    getCarPage: tryCatch(async (req, res) => {
         const page = parseInt(req.query.page);
         const perPage = parseInt(req.query.per_page);
         let types = req.query.type;
         let brands = req.query.brand;
         const maxPrice = req.query.max_price;
         const offset = (page - 1) * perPage;
-        if(!(brands instanceof Array) && brands != undefined) {
+        if (!(brands instanceof Array) && brands != undefined) {
             brands = [brands];
         }
-        if(!(types instanceof Array) && types != undefined) {
+        if (!(types instanceof Array) && types != undefined) {
             types = [types];
         }
-        const data = await Car.getCarPage(brands,types,maxPrice,perPage,offset);
+        const data = await Car.getCarPage(brands, types, maxPrice, perPage, offset);
         res.json(data);
     }),
 }
--- models/user.js
+++ models/user.js
@@ -1,11 +1,17 @@
-const { response } = require('express');
 const dbExecute = require('../utils/dbExecute');
-const { TableName } = require('pg-promise');
 const tableName = 'User';
 
 module.exports = class User {
-    constructor(...args) {
-        //Todo: assign value here
+    constructor(u) {
+        this.username = u.username;
+        this.password = u.password;
+        this.permission = u.permission;
+        this.id = u.id;
+        this.firstname = u.firstname;
+        this.phonenumber = u.phonenumber;
+        this.dob = u.dob;
+        this.address = u.address;
+        this.lastname = u.lastname;
     }
 
     static async getAll() {
@@ -23,4 +29,8 @@ module.exports = class User {
     static async delete(id) {
         return await dbExecute.delete(id, tableName);
     }
+    static async getById(id) {
+        return await dbExecute.getById(id, tableName);
+    }
+    //todo: add more function that system need
 }
