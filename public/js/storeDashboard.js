250410696b64307142c539341650f8a7cc3783af
21120170 <phuxuan.px2003@gmail.com>
2024-01-20 13:45:01 +0700

    Store manager View

--- public/js/storeDashboard.js
+++ public/js/storeDashboard.js
@@ -8,6 +8,8 @@ let carName = [];
 let apQuantity = [];
 let apName = [];
 
+let allItems;
+
 async function getData() {
     try {
         let url = '/api/store/items';
@@ -15,13 +17,13 @@ async function getData() {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        let data = await response.json();
-        data.car.forEach(e => {
+        allItems = await response.json();
+        allItems.car.forEach(e => {
             carName.push(e.car_name);
             carQuantity.push(e.quantity)
             noCar += e.quantity;
         });
-        data.ap.forEach(e => {
+        allItems.ap.forEach(e => {
             apName.push(e.name);
             apQuantity.push(e.quantity)
             noAp += e.quantity;
@@ -180,12 +182,58 @@ function displayApChart() {
 }
 
 async function displayDashboard() {
-    await getData();
+    $('#mainContent').html(` <div class="row justify-content-center">
+        <canvas id = "carChart" style = "width:100%;height:300px"> </>
+                </div >
+                <div class="row justify-content-center">
+                    <canvas id="apChart" style="width:100%;height:300px"></canvas>
+                </div>
+
+                <div class="row justify-content-evenly">
+                    <div class="card m-3 col-5">
+                        <div class="card-body">
+                            <div class="row align-items-center flex-row">
+                                <div style="font-size: 12px; color:red"><i>*the most remaining car</i></div>
+                                <div class="col-5">
+                                    <h2 class="mb-0 d-flex align-items-center"><span id="noMostCar">86.4</span><span
+                                            class="dot bg-green d-inline-block ml-3"></span></h2><span
+                                        class="text-muted text-uppercase small" id="nameMostCar">Work hours</span>
+                                    <hr><small class="text-muted" id="priceMostCar"></small>
+                                </div>
+                                <div class="col-7">
+                                    <canvas id="mostCar" style="height:50px;width:50px"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card m-3 col-5">
+                        <div class="card-body">
+                            <div class="row align-items-center flex-row">
+                                <div style="font-size: 12px; color:red"><i>*the most remaining auto part</i></div>
+                                <div class="col-5">
+                                    <h2 class="mb-0 d-flex align-items-center"><span id="noMostAp">86.4</span><span
+                                            class="dot bg-green d-inline-block ml-3"></span></h2><span
+                                        class="text-muted text-uppercase small" id="nameMostAp">Work hours</span>
+                                    <hr><small class="text-muted" id="priceMostAp">Lorem ipsum dolor sit</small>
+                                </div>
+                                <div class="col-7">
+                                    <canvas id="mostAp" style="height:50px;width:50px"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+`);
     displayCarChart();
     displayApChart();
 }
 
-displayDashboard();
+async function initWindow() {
+    await getData();
+    displayMode('dashboard');
+}
+
+initWindow();
 
 //getMode
 //displaymode
@@ -210,9 +258,90 @@ function displayMode(mode) {
 }
 
 function displayCar() {
+    allItems.car.forEach(e => {
+        $('#mainContent').append(`
+    <div class="job-box d-md-flex align-items-center justify-content-between mb-30">
+        <div class="job-left my-4 d-md-flex align-items-center flex-wrap">
+            <div
+                class="img-holder mr-md-4 mb-md-0 mb-4 mx-auto mx-md-0 d-md-none d-lg-flex">
+                ${e.quantity}
+            </div>
+            <div class="job-content">
+                <h5 class="text-center text-md-left">${e.car_name}</h5>
+                <ul class="d-md-flex flex-wrap text-capitalize ff-open-sans">
+                    <li class="mr-md-4">
+                        <i class="zmdi zmdi-car-wash"></i>${e.brand}
+                    </li>
+                    <li class="mr-md-4 ms-2">
+                        <i class="zmdi zmdi-money mr-2"></i>${e.price}
+                    </li>
+                    <li class="mr-md-4 ms-2">
+                        <i class="zmdi zmdi-drink"></i> ${e.type}
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-1">
+            <div class="job-right my-4 flex-shrink-0">
+                <a href="#" class="btn d-block w-100 d-sm-inline-block btn-light"><i
+                    class="fa-solid fa-pen-to-square" style="color: #0b488e;"></i></a>
+            </div>
+            <div class="job-right my-4 flex-shrink-0">
+                <a href="#" class="btn d-block w-100 d-sm-inline-block btn-light"><i
+                    class="fa-solid fa-trash" style="color: #7e1811;"></i></a>
+            </div>
+        </div>
 
+    </div>
+`)
+    })
+        ;
 }
 
 function displayAp() {
+    allItems.ap.forEach(e => {
+        $('#mainContent').append(`
+    <div class="job-box d-md-flex align-items-center justify-content-between mb-30">
+        <div class="job-left my-4 d-md-flex align-items-center flex-wrap">
+            <div
+                class="img-holder mr-md-4 mb-md-0 mb-4 mx-auto mx-md-0 d-md-none d-lg-flex">
+                ${e.quantity}
+            </div>
+            <div class="job-content">
+                <h5 class="text-center text-md-left">${e.name}</h5>
+                <ul class="d-md-flex flex-wrap text-capitalize ff-open-sans">
+                    <li class="mr-md-4">
+                        <i class="zmdi zmdi-car-wash"></i>${e.supplier}
+                    </li>
+                    <li class="mr-md-4 ms-2">
+                        <i class="zmdi zmdi-money mr-2"></i>${e.price}
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-1">
+            <div class="job-right my-4 flex-shrink-0">
+                <a href="#" class="btn d-block w-100 d-sm-inline-block btn-light"><i
+                    class="fa-solid fa-pen-to-square" style="color: #0b488e;"></i></a>
+            </div>
+            <div class="job-right my-4 flex-shrink-0">
+                <a href="#" class="btn d-block w-100 d-sm-inline-block btn-light"><i
+                    class="fa-solid fa-trash" style="color: #7e1811;"></i></a>
+            </div>
+        </div>
 
+    </div>
+`)
+    })
+        ;
 }
+
+$('#dashboard').click(() => {
+    displayMode('dashboard');
+})
+$('#car').click(() => {
+    displayMode('car');
+})
+$('#ap').click(() => {
+    displayMode('ap');
+})
