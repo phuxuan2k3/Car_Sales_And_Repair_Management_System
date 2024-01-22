be01b5e4b843b15655e82d9508c3821018c8143d
21120170 <phuxuan.px2003@gmail.com>
2024-01-22 22:22:57 +0700

    notify before delete

--- public/js/storeAp.js
+++ public/js/storeAp.js
@@ -13,32 +13,38 @@ $('.delete').click(async function (e) {
     console.log($(e.target));
     const url = `api/ap`;
     const data = { id };
-    try {
-        const response = await fetch(url, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
+    $('#sureDelete').click(async () => {
+        try {
+            const response = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const result = await response.json();
+
+            displayDeleteResult(result);
+        } catch (error) {
+            console.error('Error:', error);
+            throw error;
         }
-
-        const result = await response.json();
-        displayDeleteResult(result);
-    } catch (error) {
-        console.error('Error:', error);
-        throw error;
-    }
+    })
 })
 
 function displayDeleteResult(result) {
     $('.toast-body').text(result.message);
     if (result.success) {
         $('.toast-header').css('background-color', 'green');
-        $('.toast-body').append('<p class="btn btn-success">&#10;&#13;<a href="/ap" style="all:unset;color:white">Click here to refresh page.</a></p>');
+        $('.toast-body').append('<p">&#10;&#13;We will refresh after 5 seconds<p>');
+        setTimeout(() => {
+            window.location.href = '/ap';
+        }, 5000);
     } else {
         $('.toast-header').css('background-color', 'red');
     }
--- public/js/storeCar.js
+++ public/js/storeCar.js
@@ -9,36 +9,42 @@ $('#ap').click(() => {
 })
 
 $('.delete').click(async function (e) {
+
     const id = $(e.target).data('id');
     console.log($(e.target));
     const url = `api/car`;
     const data = { id };
-    try {
-        const response = await fetch(url, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
+    $('#sureDelete').click(async () => {
+        try {
+            const response = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            displayDeleteResult(result);
+        } catch (error) {
+            console.error('Error:', error);
+            throw error;
         }
-
-        const result = await response.json();
-        displayDeleteResult(result);
-    } catch (error) {
-        console.error('Error:', error);
-        throw error;
-    }
+    })
 })
 
 function displayDeleteResult(result) {
     $('.toast-body').text(result.message);
     if (result.success) {
         $('.toast-header').css('background-color', 'green');
-        $('.toast-body').append('<p class="btn btn-success">&#10;&#13;<a href="/car" style="all:unset;color:white">Click here to refresh page.</a></p>');
+        $('.toast-body').append('<p">&#10;&#13;We will refresh after 5 seconds<p>');
+        setTimeout(() => {
+            window.location.href = '/car';
+        }, 5000);
     } else {
         $('.toast-header').css('background-color', 'red');
     }
